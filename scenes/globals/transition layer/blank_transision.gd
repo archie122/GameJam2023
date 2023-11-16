b6c7a3617bf7555678b6744d1e8b0c56f67685eb
Archie <110494298+archie122@users.noreply.github.com>
2023-11-15 21:05:58 -0700

    Revert "Added a map"
    This reverts commit b50304dc8b5d8de600a32af54b71649fd40e13c6.

--- scenes/globals/transition layer/blank_transision.gd
+++ scenes/globals/transition layer/blank_transision.gd
@@ -1,14 +1,10 @@
 extends CanvasLayer
 
 func change_scean(tarrget:String) -> void:
-	show()
 	$".".layer = 2
 	$AnimationPlayer.play("fade to black")
 	
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("revert")
-	
 	get_tree().change_scene_to_file(tarrget)
-	await $AnimationPlayer.animation_finished
-	$".".layer = 0
-	hide()
+	
